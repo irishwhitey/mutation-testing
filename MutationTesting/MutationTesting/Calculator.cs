94a71bfcb77be6a9ff2478f756af21cf31b89098
martin <irishwhitey@hotmail.com>
2023-03-26 00:19:48 +0000

    add untested code

--- MutationTesting/MutationTesting/Calculator.cs
+++ MutationTesting/MutationTesting/Calculator.cs
@@ -4,8 +4,7 @@ public class Calculator
 {
     public int Add(int i, int i1)
     {
-        if (i == 2) return 6;
-        if (i == 4) return 7;
-        return 3;
+        if (i < i1) return 3;
+        return 8;
     }
 }
--- MutationTesting/Tests/Class1.cs
+++ MutationTesting/Tests/Class1.cs
@@ -1,5 +1,4 @@
-﻿using System.Dynamic;
-using Xunit;
+﻿using Xunit;
 
 namespace Tests;
 
